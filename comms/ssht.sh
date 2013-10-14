bfda20ad3bb27cec9105ebb04b0708bbdc2e6619
Ximin Luo <infinity0@gmx.com>
2013-10-14 18:18:19 +0100

    - ssht: log to ~/.local/share/autossh instead of ~/.ssh to avoid polluting config backups

--- comms/ssht.sh
+++ comms/ssht.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Simple wrapper around autossh that automatically:
 # - adds a remote command if "#RemoteCommand" is defined in ssh_config, else -N
-# - sets AUTOSSH_LOGFILE to ~/.ssh/autossh.log
+# - sets AUTOSSH_LOGFILE to ~/.local/share/autossh/autossh.log
 
 get_ssh_config_opt() {
 	sed -nr '/^Host\s+(.*\s+)?'"$1"'(\s+|$)/,/^\s*$/p' ~/.ssh/config | sed -nr -e 's/^'"$2"'\s+(.*)/\1/gp'
@@ -16,7 +16,8 @@ get_host_from_ssh_cmdline() {
 	done
 }
 
-export AUTOSSH_LOGFILE="$HOME/.ssh/autossh.log"
+mkdir -p "$HOME/.local/share/autossh"
+export AUTOSSH_LOGFILE="$HOME/.local/share/autossh/autossh.log"
 
 HOST=$(get_host_from_ssh_cmdline "$@")
 # TODO: detect hostname properly, inc. parse user@host:port correctly
