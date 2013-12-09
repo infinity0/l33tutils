0990af65fd8e66bd091db539646227adc37d3acc
Ximin Luo <infinity0@gmx.com>
2013-12-09 16:08:51 +0000

    imap4client.py - basic repl for reading recent email, intentionally limited in features. this is the first step of moving my email password off my phone.

--- ops/imap4client.py
+++ ops/imap4client.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
+# Copyright (c) Ximin Luo
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 
 """
-Simple IMAP4 client which displays the subjects of all messages in a
-particular mailbox.
+Simple read-only IMAP4 REPL.
 """
 
+import argparse
+import shlex
+import subprocess
 import sys
 
 from twisted.internet import protocol
@@ -100,7 +103,7 @@ class SimpleIMAP4ClientFactory(protocol.ClientFactory):
 
 
 
-def cbServerGreeting(proto, username, password):
+def cbServerGreeting(proto, username, password, mbox):
     """
     Initial callback - invoked after the server sends us its greet message.
     """
@@ -114,7 +117,7 @@ def cbServerGreeting(proto, username, password):
 
     # Try to authenticate securely
     return proto.authenticate(password
-        ).addCallback(cbAuthentication, proto
+        ).addCallback(cbAuthentication, proto, mbox
         ).addErrback(ebAuthentication, proto, username, password
         )
 
@@ -128,16 +131,20 @@ def ebConnection(reason):
     return reason
 
 
-def cbAuthentication(result, proto):
+def cbAuthentication(result, proto, mbox):
     """
     Callback after authentication has succeeded.
 
     Lists a bunch of mailboxes.
     """
-    return proto.list("", "*"
-        ).addCallback(cbMailboxList, proto
-        )
-
+    if mbox:
+        return proto.examine(mbox
+            ).addCallback(cbExamineMbox, proto
+            )
+    else:
+        return proto.list("", "*"
+            ).addCallback(cbMailboxList, proto
+            )
 
 def ebAuthentication(failure, proto, username, password):
     """
@@ -149,22 +156,7 @@ def ebAuthentication(failure, proto, username, password):
     If you are trying to connect to your Gmail account, you will be here!
     """
     failure.trap(imap4.NoSupportedAuthentication)
-    return proto.prompt(
-        "No secure authentication available. Login insecurely? (y/N) "
-        ).addCallback(cbInsecureLogin, proto, username, password
-        )
-
-
-def cbInsecureLogin(result, proto, username, password):
-    """
-    Callback for "insecure-login" prompt.
-    """
-    if result.lower() == "y":
-        # If they said yes, do it.
-        return proto.login(username, password
-            ).addCallback(cbAuthentication, proto
-            )
-    return defer.fail(Exception("Login failed for security reasons."))
+    return defer.fail(Exception("No secure authentication available; abort"))
 
 
 def cbMailboxList(result, proto):
@@ -172,10 +164,10 @@ def cbMailboxList(result, proto):
     Callback invoked when a list of mailboxes has been retrieved.
     """
     result = [e[2] for e in result]
-    s = '\n'.join(['%d. %s' % (n + 1, m) for (n, m) in zip(range(len(result)), result)])
+    s = '\n'.join(['%d. %s' % (n + 1, m) for (n, m) in enumerate(result)])
     if not s:
         return defer.fail(Exception("No mailboxes exist on server!"))
-    return proto.prompt(s + "\nWhich mailbox? [1] "
+    return proto.prompt(s + "\nWhich mailbox? [Quit] "
         ).addCallback(cbPickMailbox, proto, result
         )
 
@@ -184,38 +176,88 @@ def cbPickMailbox(result, proto, mboxes):
     """
     When the user selects a mailbox, "examine" it.
     """
-    mbox = mboxes[int(result or '1') - 1]
-    return proto.examine(mbox
-        ).addCallback(cbExamineMbox, proto
-        )
+    if result and result != 'q':
+        mbox = mboxes[int(result or '1') - 1]
+        return proto.examine(mbox
+            ).addCallback(cbExamineMbox, proto
+            )
+    else:
+        proto.display("Goodbye\n")
+        return proto.logout()
 
 
 def cbExamineMbox(result, proto):
     """
     Callback invoked when examine command completes.
 
-    Retrieve the subject header of every message in the mailbox.
+    Search for recent messages.
     """
-    return proto.fetchSpecific('1:*',
-                               headerType='HEADER.FIELDS',
-                               headerArgs=['SUBJECT'],
-        ).addCallback(cbFetch, proto
+    return proto.search(imap4.Query(sorted=1, recent=1)
+        ).addCallback(cbRecent, proto
         )
+
+
+def cbRecent(result, proto):
+    """
+    Callback invoked when search command completes.
+
+    Retrieve the subject header of every message in the result.
+    """
+    if result:
+        s = imap4.MessageSet(zip(result, result))
+        return proto.fetchSpecific(s,
+                                   headerType='HEADER.FIELDS',
+                                   headerArgs=['FROM', 'SUBJECT', 'DATE'],
+            ).addCallback(cbFetch, proto
+            )
+    else:
+        proto.display("No recent messages; Goodbye\n")
+        return proto.logout()
 
 
 def cbFetch(result, proto):
     """
-    Finally, display headers.
+    Display headers, then prompt for message(s) to fetch.
     """
     if result:
         keys = result.keys()
         keys.sort()
         for k in keys:
-            proto.display('%s %s' % (k, result[k][0][2]))
-    else:
-        print "Hey, an empty mailbox!"
+            proto.display("## %s\n%s" % (k, result[k][0][2]))
 
-    return proto.logout()
+        return proto.prompt('Which message(s)? ("1" or "1,2" or "1:3") [Quit] '
+            ).addCallback(cbPickMessage, proto
+            )
+    else:
+        proto.display("Empty header result; return to recent.\n")
+        return cbExamineMbox(None, proto)
+
+
+def cbPickMessage(result, proto):
+    """
+    Fetch a message, then display it.
+    """
+    if result and result != 'q':
+        return proto.fetchMessage(result).addCallback(cbFetchMessage, proto)
+    else:
+        proto.display("Goodbye\n")
+        return proto.logout()
+
+
+def cbFetchMessage(result, proto):
+    """
+    Display a message, then loop back to recent messages.
+    """
+    if result:
+        keys = result.keys()
+        keys.sort()
+        for k in keys:
+            proto.display("## %s\n%s\n" % (k, result[k]['RFC822']))
+
+        return cbExamineMbox(None, proto)
+    else:
+        proto.display("Empty body result; return to recent.\n")
+        return cbExamineMbox(None, proto)
 
 
 def cbClose(result):
@@ -227,25 +269,43 @@ def cbClose(result):
 
 
 def main():
-    hostname = raw_input('IMAP4 Server Hostname: ')
-    port = raw_input('IMAP4 Server Port (the default is 143, 993 uses SSL): ')
-    username = raw_input('IMAP4 Username: ')
-    password = util.getPassword('IMAP4 Password: ')
+    parser = argparse.ArgumentParser(
+        description="""Check your INBOX via IMAP-SSL.
 
-    onConn = defer.Deferred(
-        ).addCallback(cbServerGreeting, username, password
-        ).addErrback(ebConnection
-        ).addBoth(cbClose)
+Passwords are obtained via secret-tool(1). You can put this tool, which only
+*reads* your email, behind a more restricted form of access, such as an
+authenticated web interface, so that you don't need to give an insecure email
+client (such as on your smartphone) your account password, and write-access
+along with it.
+""")
+    parser.add_argument('user', metavar='NAME',
+        help="user to authenticate as")
+    parser.add_argument('host', metavar='HOST',
+        help="host to connect to")
+    parser.add_argument('port', metavar='PORT',
+        help="port to connect to",
+        nargs='?', default=993, type=int)
+    parser.add_argument('-m', '--mbox', metavar='MBOX',
+        help="IMAP mbox (i.e. folder/directory) to check, default %(default)s. "
+        "set empty to list all mailboxes and then prompt for one to use.",
+        default="INBOX")
+    parser.add_argument('-s', '--secret', metavar='ARGS',
+        help="query for `secret-tool(1) lookup`, parsed with shlex in POSIX "
+        "mode. If empty, we use \"hostname imap://$host username $user\", "
+        "which is compatible with passwords stored via mozilla-gnome-keyring.")
 
-    factory = SimpleIMAP4ClientFactory(username, onConn)
+    args = parser.parse_args()
+    secret = args.secret or "hostname imap://%s username %s" % (args.host, args.user)
+    password = subprocess.check_output(["secret-tool", "lookup"] + shlex.split(secret))
+
+    onConn = defer.Deferred()
+    onConn = onConn.addCallbacks(cbServerGreeting, ebConnection, callbackArgs=(args.user, password, args.mbox))
+    onConn = onConn.addBoth(cbClose)
+
+    factory = SimpleIMAP4ClientFactory(args.user, onConn)
 
     from twisted.internet import reactor
-    if port == '993':
-        reactor.connectSSL(hostname, int(port), factory, ssl.ClientContextFactory())
-    else:
-        if not port:
-            port = 143
-        reactor.connectTCP(hostname, int(port), factory)
+    reactor.connectSSL(args.host, args.port, factory, ssl.ClientContextFactory())
     reactor.run()
 
 
