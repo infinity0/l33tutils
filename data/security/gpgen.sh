90abeb221b60be1359269d5766f31afbbd05fc4b
Ximin Luo <infinity0@pwned.gg>
2014-12-11 01:16:35 +0100

    gpgen: update to GPG 2.1 and EdDSA keys

--- data/security/gpgen.sh
+++ data/security/gpgen.sh
@@ -11,13 +11,22 @@
 # TODO: sub-script to update expiry dates
 # TODO: sub-script to generate new encryption subkey, export only latest one
 
-KEYTYPE=8 # RSA
-KEYLEN=4096
+KEYTYPE_CSA=11 # ECC
+KEYPARAM_CSA="1
+y" # ECC Curve255, then pass gnupg 2.1 warning
 EXPIRE_CSA=1y
+
+KEYTYPE_E=5 # Elgamal
+KEYPARAM_E=4096 # Elgamal keylength
 EXPIRE_E=90d
 
 NAME="$1"; shift
 test "${#NAME}" -ge 5 || { echo >&2 "Usage: $0 NAME [EMAIL ...]"; exit 2; }
+
+if [ "$(gpg2 --version | head -n1)" != "gpg (GnuPG) 2.1.0" ]; then
+	echo >&2 "Unexpected version (expected 2.1.0); abort"
+	exit 2
+fi
 
 echo "Creating key for $NAME with emails: $@"
 echo "You should be doing this on an offline airgapped secure machine."
@@ -27,21 +36,21 @@ read -p "Press enter to continue or ctrl-c to abort" x
 set -e
 set -x
 
-genlog=".$(basename "$0").$$.$(date +%s)"
+prefix=gpgen-home
+rm -rf "$prefix"
+genlog="$prefix/$(basename "$0").$$.$(date +%s)"
 gpgex() {
     name="$1"; shift
+    mkdir -p gpgen-home
     gpg2 --no-default-keyring --expert --command-fd 0 --status-fd 3 \
-      --keyring ./pubring.gpg --secret-keyring ./secring.gpg --trustdb-name ./trustdb.gpg \
-       "$@" \
+      --homedir="$prefix" "$@" \
       3>&1 1>/dev/null | tee -a "$genlog.$name.log"
 }
 
-gpgex master --gen-key <<EOF
-$KEYTYPE
-s
-e
-q
-$KEYLEN
+gpgex master --full-gen-key <<EOF
+$KEYTYPE_CSA
+=c
+$KEYPARAM_CSA
 $EXPIRE_CSA
 y
 $NAME
@@ -58,28 +67,22 @@ cat <<EOF
 primary
 1
 addkey
-$KEYTYPE
-e
-q
-$KEYLEN
+$KEYTYPE_CSA
+=s
+$KEYPARAM_CSA
 $EXPIRE_CSA
 y
 y
 addkey
-$KEYTYPE
-s
-e
-a
-q
-$KEYLEN
+$KEYTYPE_CSA
+=a
+$KEYPARAM_CSA
 $EXPIRE_CSA
 y
 y
 addkey
-$KEYTYPE
-s
-q
-$KEYLEN
+$KEYTYPE_E
+$KEYPARAM_E
 $EXPIRE_E
 y
 y
@@ -98,16 +101,11 @@ echo save
 
 } | gpgex subkeys --edit-key "$fingerprint"
 
-gpgex export -o "$fingerprint.sec.gpg" --export-secret-subkey "$fingerprint"
-gpgex export -o "$fingerprint.pub.gpg" --export "$fingerprint"
-gpgex export -o "$fingerprint.rev.gpg" --gen-revoke "$fingerprint" <<EOF
-y
-0
-
-y
-EOF
+gpgex export -o "$fingerprint.sec.asc" -a --export-secret-subkey "$fingerprint"
+gpgex export -o "$fingerprint.pub.asc" -a --export "${fingerprint:32:8}" # use whol fpr when #1787 is fixed
+cp "$prefix/openpgp-revocs.d/$fingerprint.rev" "$fingerprint.rev.asc"
 
 set +x
-echo "Key generation complete. Store ./{pub,sec}ring.gpg somewhere offline."
+echo "Key generation complete. Store ./$prefix somewhere offline."
 echo "Store $fingerprint.rev.gpg somewhere safe offline and don't lose it."
 echo "You may import $fingerprint.{sec,pub}.gpg to an online gpg installation."
