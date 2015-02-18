d66cde1d5a974e6c435ed77bd51b8abe6c5fc32c
Ximin Luo <infinity0@pwned.gg>
2015-02-18 14:34:29 +0100

    dyndns: a convenience wrapper around nsupdate.sh

---
+++ ops/dyndns.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Simple wrapper around nsupdate.
+#
+# Usage: $0 <NAME.conf>
+#
+# NAME.conf is a shell snippet that defines:
+# NS_AUTH - authoritative server to submit updates to
+# NS_NAME - hostname to perform the update on
+# NS_ZONE - zone that the hostname belongs to
+#
+# NAME.key must exist and contain a valid key, see nsupdate(1) for details
+# NAME.addr should be writeable/createable. It will hold your current IP address.
+
+CONF="$1"
+test -f "$CONF" || { echo >&2 "not a file: $CONF"; exit 2; }
+
+. "$(readlink -f "$CONF")"
+test -n "$NS_AUTH" || { echo >&2 "conf didn't define NS_AUTH"; exit 1; }
+test -n "$NS_NAME" || { echo >&2 "conf didn't define NS_NAME"; exit 1; }
+test -n "$NS_ZONE" || { echo >&2 "conf didn't define NS_ZONE"; exit 1; }
+
+BASE="${CONF%.conf}"
+
+IPADDR="$(curl -s https://wtfismyip.com/text || curl -s https://api.ipify.org/?format=text || curl -s https://icanhazip.com/)"
+test -n "$IPADDR" || exit 1
+OLDIP="$(cat "$BASE.addr")"
+test "$IPADDR" = "$OLDIP" && exit 0
+echo "$IPADDR" > "$BASE.addr"
+
+nsupdate -k "$BASE.key" -v <<EOF
+server $NS_AUTH
+zone $NS_ZONE
+update delete $NS_NAME. A
+update add $NS_NAME. 1200 A $IPADDR
+send
+EOF
