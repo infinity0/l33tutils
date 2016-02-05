c22618653c25426840bd6fd1e2c4c554d290cf08
Ximin Luo <infinity0@pwned.gg>
2016-02-05 16:02:44 +0100

    dyndns: make the script more robust to service failures

--- ops/dyndns.sh
+++ ops/dyndns.sh
@@ -10,6 +10,8 @@
 #
 # NAME.key must exist and contain a valid key, see nsupdate(1) for details
 # NAME.addr should be writeable/createable. It will hold your current IP address.
+#
+# TODO: figure out a way to run this when ip addr (*of the router*) changes
 
 CONF="$1"
 test -f "$CONF" || { echo >&2 "not a file: $CONF"; exit 2; }
@@ -20,13 +22,27 @@ test -n "$NS_NAME" || { echo >&2 "conf didn't define NS_NAME"; exit 1; }
 test -n "$NS_ZONE" || { echo >&2 "conf didn't define NS_ZONE"; exit 1; }
 
 BASE="${CONF%.conf}"
-
-IPADDR="$(curl -s https://wtfismyip.com/text || curl -s https://api.ipify.org/?format=text || curl -s https://icanhazip.com/)"
-if ! echo "$IPADDR" | grep -q -P '^\d+\.\d+\.\d+\.\d+$' -; then echo >&2 "error getting IP: $IPADDR"; exit 1; fi
 OLDIP="$(cat "$BASE.addr")"
-test "$IPADDR" = "$OLDIP" && exit 0
-echo "$IPADDR" > "$BASE.addr"
 
+get_ip() {
+	eval "$1" | tee "$BASE.addr.tmp" | grep -q -P '^\d+\.\d+\.\d+\.\d+$' -
+}
+
+shuf <<EOF | while read x; do if get_ip "$x"; then break; fi done
+curl -s https://wtfismyip.com/text
+curl -s https://api.ipify.org/?format=text
+curl -s https://icanhazip.com
+EOF
+
+IPADDR="$(cat "$BASE.addr.tmp")"
+if ! echo "$IPADDR" | grep -q -P '^\d+\.\d+\.\d+\.\d+$' -; then
+	echo >&2 "error getting IP: $IPADDR"
+	rm "$BASE.addr.tmp"
+	exit 1
+fi
+
+test "$IPADDR" = "$OLDIP" && exit 0
+mv -f "$BASE.addr.tmp" "$BASE.addr"
 nsupdate -k "$BASE.key" -v <<EOF
 server $NS_AUTH
 zone $NS_ZONE
