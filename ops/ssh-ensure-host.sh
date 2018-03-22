360a9e0e2193075b70202b609430e3804d10e3f0
Ximin Luo <infinity0@pwned.gg>
2018-03-22 12:52:40 +0100

    ssh-ensure-host: add an ssh key by host

---
+++ ops/ssh-ensure-host.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Add an ssh key by hostname, if it is not already added.
+
+host="$1"
+shift
+keyfile=$(ssh -G "$host" | sed -ne 's/identityfile //p' | { read x y; echo $x; })
+keyfile="${keyfile/#\~/$HOME}"
+if ! ssh-add -l | grep -wF "$keyfile"; then
+    ssh-add -t3600 "$@" "$keyfile"
+    ssh-add -l | grep -wF "$keyfile"
+fi
