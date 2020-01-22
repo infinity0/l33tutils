9ac158e5a6c67a8e45d36f3ba7829ee810869680
Ximin Luo <infinity0@pwned.gg>
2020-01-22 00:56:21 +0000

    pdf-amend: support editing multiple pages at once

--- ops/pdf-amend.py
+++ ops/pdf-amend.py
@@ -5,6 +5,7 @@ Amend PDFs more easily, e.g. to sign contracts with.
 Requires: python3-pypdf2, imagemagick, gimp
 """
 import argparse
+import contextlib
 import os
 import subprocess
 import sys
@@ -19,35 +20,47 @@ def main(args):
     parser = argparse.ArgumentParser(
         description='Amend one page of a pdf document.')
     parser.add_argument('orig_pdf', type=str)
-    parser.add_argument('page_num_to_amend', type=int)
-    parser.add_argument('-a', '--amend-with', dest='amend_with')
+    parser.add_argument('page_num_to_amend', type=int, nargs='+')
+    parser.add_argument('-a', '--amend-with', dest='amend_with', action='append')
     parser.add_argument('-o', '--output')
     args = parser.parse_args(args)
 
-    page = args.page_num_to_amend
-    pidx = page - 1
     main, ext = os.path.splitext(args.orig_pdf)
+    pages = args.page_num_to_amend
+    amend_withs = args.amend_with
+    if not amend_withs:
+        amend_withs = ["%s.page-%s.amended%s" % (main, page, ext) for page in pages]
+    elif len(amend_withs) != len(pages):
+        raise ValueError("there must be an equal number of --amend-with as page_num_to_amend")
     new_pdf = args.output or "%s.amended%s" % (main, ext)
-    amend_with = args.amend_with or "%s.page-%s.amended%s" % (main, page, ext)
+    pairs = sorted(zip(pages, amend_withs))
 
-    if os.path.exists(amend_with):
-        print("using existing amended page %s" % amend_with)
-    else:
-        print("%s does not exist; I will now open GIMP so you can edit page #%s in that file." % (amend_with, page))
-        print("After you have made your edits, do File -> Overwrite %s, then exit GIMP." % amend_with)
-        print("(It's safe to 'Discard Changes' after Overwrite.)")
-        input("Press enter to continue and run GIMP ")
-        amend_with_png = "%s.page-%s.amended.png" % (main, page)
-        C(("convert -density %s -units pixelsperinch" % DENSITY_DPI).split() +
-          ['%s[%s]' % (args.orig_pdf, pidx), amend_with_png])
-        C(["gimp", amend_with_png])
-        C(["convert", amend_with_png, amend_with])
+    for (page, amend_with) in pairs:
+        pidx = page - 1
+        if os.path.exists(amend_with):
+            print("using existing amended page %s" % amend_with)
+        else:
+            print("%s does not exist; I will now open GIMP so you can edit page #%s in that file." % (amend_with, page))
+            print("After you have made your edits, do File -> Overwrite %s, then exit GIMP." % amend_with)
+            print("(It's safe to 'Discard Changes' after Overwrite.)")
+            input("Press enter to continue and run GIMP ")
+            amend_with_png = "%s.page-%s.amended.png" % (main, page)
+            C(("convert -density %s -units pixelsperinch" % DENSITY_DPI).split() +
+              ['%s[%s]' % (args.orig_pdf, pidx), amend_with_png])
+            C(["gimp", amend_with_png])
+            C(["convert", amend_with_png, amend_with])
 
-    with open(args.orig_pdf, 'rb') as orig, open(amend_with, 'rb') as new:
+    with contextlib.ExitStack() as stack:
+        orig = stack.enter_context(open(args.orig_pdf, 'rb'))
         pdf = PdfFileMerger()
-        pdf.merge(0, orig, pages = PageRange("0:%s" % pidx))
-        pdf.merge(pidx, new)
-        pdf.merge(pidx+1, orig, pages = PageRange("%s:" % (pidx+1)))
+        pidx = 0
+        for (page, amend_with) in pairs:
+            if page > pidx + 1:
+                pdf.merge(pidx, orig, pages = PageRange("%s:%s" % (pidx, page - 1)))
+            pidx = page - 1
+            pdf.merge(pidx, stack.enter_context(open(amend_with, 'rb')))
+            pidx += 1
+        pdf.merge(pidx, orig, pages = PageRange("%s:" % pidx))
         pdf.write(new_pdf)
         print("wrote %s" % new_pdf)
 
