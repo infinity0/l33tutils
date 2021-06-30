c56aae21b79539a26def04bfc296b5c98160056c
Ximin Luo <infinity0@pwned.gg>
2021-06-30 21:24:40 +0100

    dyndns: support ipv4 & ipv6 simultaneously, merge in gandi-livedns functionality

--- ops/dyndns.sh
+++ ops/dyndns.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
-# Simple wrapper around nsupdate. Requires stun(1) from the stun-client package.
+# Simple wrapper around nsupdate.
 #
 # Usage: $0 <NAME.conf> <FAILURE CMD>
 #
-# NAME.conf is a shell snippet that defines:
-# NS_AUTH - authoritative server to submit updates to
-# NS_NAME - hostname to perform the update on
-# NS_ZONE - zone that the hostname belongs to
-# IPV - version of IP to use, either 4 or 6
-#
-# NAME.key must exist and contain a valid key, see nsupdate(1) for details
-# NAME.addr should be writeable/createable. It will hold your current IP address.
+# Files:
+# NAME.conf
+#   Shell snippet that defines:
+#   NS_ZONE
+#     FQDN zone that the hostname belongs to, e.g. "example.com"
+#   NS_NAME
+#     hostname to perform the update on, e.g. "mysub" to update
+#     "mysub.example.com" or "@" to update "example.com".
+#   IPV
+#     IP versions to use, one of {4,6,46}. If using IPv4 then we require stun(1)
+#     from the stun-client package.
+#   METHOD
+#     DNS update method to use, in the following form:
+#     nsupdate <NS_AUTH>
+#       Submit updates via nsupdate(1). Args:
+#       NS_AUTH authoritative server to submit updates to
+#     gandi-livedns
+#       Submit updates to https://api.gandi.net/v5/livedns
+# NAME.key
+#   Relevant key for your update METHOD. For example, a nsupdate(1) key or a
+#   gandi-livedns API key.
+# NAME.addr4/addr6
+#   Must be writeable. It will hold your current IP address.
 #
 # TODO: figure out a way to run this when ip addr (*of the router*) changes
 # TODO: queue prev results and only change if multiple sources agree
@@ -21,32 +36,54 @@ shift
 test -f "$CONF" || { echo >&2 "not a file: $CONF"; exit 2; }
 
 . "$(readlink -f "$CONF")"
-test -n "$NS_AUTH" || { echo >&2 "conf didn't define NS_AUTH"; exit 1; }
+test -n "$METHOD" || { echo >&2 "conf didn't define METHOD"; exit 1; }
 test -n "$NS_NAME" || { echo >&2 "conf didn't define NS_NAME"; exit 1; }
 test -n "$NS_ZONE" || { echo >&2 "conf didn't define NS_ZONE"; exit 1; }
 IPV="${IPV:-4}"
 case "$IPV" in
-4) IPTYPE=A;;
-6) IPTYPE=AAAA;;
+4)	IPTYPES="addr4";;
+6)	IPTYPES="addr6";;
+46)	IPTYPES="addr4 addr6";;
 *) echo >&2 "invalid IPV: $IPV"; exit 1;;
 esac
 
 BASE="${CONF%.conf}"
-OLDIP="$(cat "$BASE.addr")"
 
 stun_ip() {
 	stun "$1" -v 2>&1 | ( err=; while read x; do
 		ipp="${x#MappedAddress = }"
 		if test -n "$err"; then echo "$x";
-		elif [ "$x" != "$ipp" ]; then echo "${ipp%:*}"; exit 0;
-		elif [ "$x" != "${x#Primary}" ]; then echo "$x"; exit 1;
+		elif [ "$x" != "$ipp" ]; then echo "${ipp%:*}"; return 0;
+		elif [ "$x" != "${x#Primary}" ]; then echo "$x"; return 1;
 		elif [ "$x" != "${x#?rror}" ]; then echo "$x"; err=1;
 		fi
-	done; exit 2; )
+	done; return 2; )
 }
 
-get_ip() {
-	eval "$1" | tee "$BASE.addr.tmp" | grep -q -P '^\d+\.\d+\.\d+\.\d+$' -
+read_ip4() {
+	eval "$1" | tee "$BASE.addr4.tmp" | grep -q -P '^\d+\.\d+\.\d+\.\d+$' -
+}
+
+get_ip4() {
+	shuf <<-EOF | while read x; do if read_ip4 "$x"; then break; fi done
+	stun_ip stun.l.google.com:19302
+	stun_ip stun.ideasip.com
+	stun_ip stun.ekiga.net
+	stun_ip stun.iptel.org
+	stun_ip stun.schlund.de
+	stun_ip stun.voiparound.com
+	stun_ip stun.voipbuster.com
+	stun_ip stun.voipstunt.com
+	stun_ip stun.voxgratia.org
+	stun_ip numb.viagenie.ca
+	stun_ip stun.counterpath.com
+	stun_ip stun.services.mozilla.com
+	stun_ip stun.sipgate.net
+	curl -s https://wtfismyip.com/text
+	curl -s https://api.ipify.org/?format=text
+	curl -s https://icanhazip.com
+	curl -s https://check.torproject.org | sed -n -re 's,.*IP address.*>([.0-9]+)<.*,\1,gp'
+	EOF
 }
 
 get_ip6() {
@@ -58,59 +95,97 @@ get_ip6() {
 		| select(.scope == "global")
 		| .local
 		| select(startswith("fd") or startswith("fc") | not)' \
-	  | head -n1 > "$BASE.addr.tmp"
+	  | head -n1 > "$BASE.addr6.tmp"
 }
 
-case "$IPV" in
-4)
-shuf <<EOF | while read x; do if get_ip "$x"; then break; fi done
-stun_ip stun.l.google.com:19302
-stun_ip stun.ideasip.com
-stun_ip stun.ekiga.net
-stun_ip stun.iptel.org
-stun_ip stun.schlund.de
-stun_ip stun.voiparound.com
-stun_ip stun.voipbuster.com
-stun_ip stun.voipstunt.com
-stun_ip stun.voxgratia.org
-stun_ip numb.viagenie.ca
-stun_ip stun.counterpath.com
-stun_ip stun.services.mozilla.com
-stun_ip stun.sipgate.net
-curl -s https://wtfismyip.com/text
-curl -s https://api.ipify.org/?format=text
-curl -s https://icanhazip.com
-curl -s https://check.torproject.org | sed -n -re 's,.*IP address.*>([.0-9]+)<.*,\1,gp'
-EOF
-	;;
-6)
-	get_ip6
-	;;
-esac
-
-IPADDR="$(cat "$BASE.addr.tmp")"
-ping -n -c1 -w1 "$IPADDR" >/dev/null
-x=$?
-if [ "$x" = 2 ]; then
-	echo >&2 "error getting IP: $IPADDR"
-	rm "$BASE.addr.tmp"
-	DAYSSINCE="$((($(date +%s) - $(stat -c%Y "$BASE.addr")) / 60 / 60 / 24))"
-	# if we have not successfully gotten our IP address in N full days, then
-	# run the failure command with probability 1 in (N+1).
-	if [ "$(shuf -i 1-"$((DAYSSINCE + 1))" -n 1)" = 1 ]; then
-		echo >&2 "running failure command"
-		( set -x; "$@" )
+changes=0
+for addr in $IPTYPES; do
+	case $addr in
+	addr4) get_ip4;;
+	addr6) get_ip6;;
+	esac
+	IPADDR="$(cat "$BASE.$addr.tmp")"
+	ping -n -c1 -w1 "$IPADDR" >/dev/null
+	x=$?
+	if [ "$x" = 2 ]; then
+		echo >&2 "error getting IP: $IPADDR"
+		rm "$BASE.$addr.tmp"
+		DAYSSINCE="$((($(date +%s) - $(stat -c%Y "$BASE.$addr")) / 60 / 60 / 24))"
+		# if we have not successfully gotten our IP address in N full days, then
+		# run the failure command with probability 1 in (N+1).
+		if [ "$(shuf -i 1-"$((DAYSSINCE + 1))" -n 1)" = 1 ]; then
+			echo >&2 "running failure command"
+			( set -x; "$@" )
+		fi
+		exit 1
 	fi
-	exit 1
-fi
-test "$IPADDR" = "$OLDIP" && { rm "$BASE.addr.tmp"; touch "$BASE.addr"; exit 0; }
-mv -f "$BASE.addr.tmp" "$BASE.addr"
+	OLDIP="$(cat "$BASE.$addr" 2>/dev/null || echo)"
+	if [ "$IPADDR" = "$OLDIP" ]; then
+		rm "$BASE.$addr.tmp"
+		touch "$BASE.$addr"
+	else
+		mv -f "$BASE.$addr.tmp" "$BASE.$addr"
+		changes=1
+	fi
+done
+if [ "$changes" = 0 ]; then exit 0; fi
 
-nsupdate -k "$BASE.key" -v <<EOF
-server $NS_AUTH
-zone $NS_ZONE
-update delete $NS_NAME. A
-update delete $NS_NAME. AAAA
-update add $NS_NAME. 1200 $IPTYPE $IPADDR
-send
-EOF
+output_addresses() {
+	local iptype ipaddr
+	for addr in $IPTYPES; do
+		case $addr in
+		addr4) iptype=A;;
+		addr6) iptype=AAAA;;
+		esac
+		ipaddr="$(cat "$BASE.$addr")"
+		"format1_${1}" "$domain" "$iptype" "$ipaddr"
+	done
+}
+
+format1_nsupdate() {
+	echo "update add $1. 1200 $2 $3"
+}
+
+format1_gandi_livedns() {
+	echo -n '{"rrset_type": "'"$2"'", "rrset_ttl": 1200, "rrset_values": ["'"$3"'"]},'
+}
+
+update() {
+	local method="$1"
+	case "$method" in
+	nsupdate)
+		local ns_auth="$2"
+		{
+		if [ "$NS_NAME" = "@" ]; then
+			local domain="$NS_ZONE"
+		else
+			local domain="$NS_NAME.$NS_ZONE"
+		fi
+		cat <<-EOF
+		server $ns_auth
+		zone $NS_ZONE
+		update delete $domain. A
+		update delete $domain. AAAA
+		EOF
+		output_addresses nsupdate
+		echo send
+		} | nsupdate -k "$BASE.key" -v
+		;;
+	gandi-livedns)
+		local payload=$(
+		echo -n '{"items": ['
+		output_addresses gandi_livedns
+		echo -n ' {}]}'
+		)
+		! curl -s -H "Authorization: Apikey $(cat "$BASE.key")" \
+		  "https://api.gandi.net/v5/livedns/domains/$NS_ZONE/records/$NS_NAME" \
+		  -X PUT \
+		  -H "Content-Type: application/json" \
+		  -d "$payload" \
+		  -w ' %{http_code}' | grep -v '201$'
+		# ! [etc] -w ' %{http_code}' | grep -v '201$' means to exit 0 only if 201 status code is given
+		;;
+	esac
+}
+
+update $METHOD
